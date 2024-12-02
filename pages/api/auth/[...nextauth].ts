bc60c55f394788e6b75a0c82f4dfb8acc520df35
Nawal <nawalsobaihi@gmai.com>
2024-12-02 23:49:30 +0300

    Final  commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GitHubProvider from 'next-auth/providers/github';
+import prisma from '../../../lib/prisma';
+import type { NextAuthOptions } from "next-auth"
+
+
+
+export const authHandler: NextAuthOptions = {
+  providers: [
+      GitHubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+      }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+}
+
+export default NextAuth(authHandler);
